71cc981af9c8a208db83535db0a1eb1b0ddd2800
David Paulsson <david@davidpaulsson.se>
2025-12-18 11:58:04 +0100

    Update robots.ts to use array for rules and force static
    Changed the 'rules' property in the robots function to an array format and added 'export const dynamic = "force-static"'. Also removed the 'host' property from the returned object.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,17 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const siteUrl = "https://www.barnvistexter.se";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
-    host: "www.barnvistexter.se",
   };
 }
